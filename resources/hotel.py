e9eca2f80ca80b75f741873e184b86b0e61c1504
Alessandro Oliveira <alessandro.ibes@gmail.com>
2022-02-17 09:50:45 -0300

    Refinamento e Tratamento de Erros

--- resources/hotel.py
+++ resources/hotel.py
@@ -33,8 +33,10 @@ class Hoteis(Resource):
 
 class Hotel(Resource):
     argumentos = reqparse.RequestParser()
-    argumentos.add_argument('nome')
-    argumentos.add_argument('estrelas')
+    argumentos.add_argument('nome', type=str, required=True,
+                            help="The field 'nome' cannot be left blank.")
+    argumentos.add_argument('estrelas', type=float, required=True,
+                            help="The field 'estrelas' cannot be left blank.")
     argumentos.add_argument('diaria')
     argumentos.add_argument('cidade')
 
@@ -51,7 +53,11 @@ class Hotel(Resource):
 
         dados = Hotel.argumentos.parse_args()
         hotel = HotelModel(hotel_id, **dados)
-        hotel.save_hotel()
+        try:
+            hotel.save_hotel()
+        except:
+            return {'message': 'An internal error ocurred trying\
+                 to save hotel.'}, 500  # Internal Server Error
         return hotel.json()
 
     def put(self, hotel_id):
@@ -62,12 +68,20 @@ class Hotel(Resource):
             hotel_encontrado.save_hotel()
             return hotel_encontrado.json(), 200      # Ok
         hotel = HotelModel(hotel_id, **dados)
-        hotel.save_hotel()
+        try:
+            hotel.save_hotel()
+        except:
+            return {'message': 'An internal error ocurred trying\
+                 to save hotel.'}, 500  # Internal Server Error
         return hotel.json(), 201                    # Created
 
     def delete(self, hotel_id):
         hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
-            hotel.delete_hotel()
+            try:
+                hotel.delete_hotel()
+            except:
+                return {'message': 'An internal error ocurred trying\
+                    to delete hotel.'}, 500  # Internal Server Error
             return {'message': 'Hotel deleted.'}
         return {'message': 'Hotel not found.'}, 404  # Not found
